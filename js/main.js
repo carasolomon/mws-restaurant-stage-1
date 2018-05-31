d3985398663560763ce1bc216bcf09bd5bf12319
Cara <Cara@Tacaras-MacBook-Pro.local>
2018-05-31 04:26:16 -0500

    "Added service worker"

--- js/main.js
+++ js/main.js
@@ -179,3 +179,18 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+// Register service worker
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/js/sw.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    },(err) => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+
